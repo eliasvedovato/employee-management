66b439a26892aee5da28056ff81c6ea18ef8f69e
Elias Vedovato <eliasvedovato@gmail.com>
2023-01-12 15:54:04 -0300

    post HTTP

--- database/controller.js
+++ database/controller.js
@@ -1,13 +1,32 @@
-import Users from "../model/user";
+import Users from '../model/user'
 
-export async function getUsers(req, res){
-  try {
-    const users = await Users.find({})
+// get : http://localhost:3000/api/users
+export async function getUsers(req, res) {
+	try {
+		const users = await Users.find({})
 
-    if(!users) return res.status(404).json({error: 'Data not found'})
+		if (!users) return res.status(404).json({ error: 'Data not found' })
 
-    res.status(200).json(users)
-  } catch(error) {
-    res.status(404).json({error: 'Error while fetching data'})
-  }
+		res.status(200).json(users)
+	} catch (error) {
+		res.status(404).json({ error: 'Error while fetching data' })
+	}
+}
+
+// post : http://localhost:3000/api/users
+export async function postUser(req, res) {
+	try {
+		//get the data from the user
+		const formData = req.body
+		if (!formData)
+			return res.status(404).json({ error: 'Form data not provided...' })
+
+    // this method create and store in the database
+    Users.create(formData, function(err, data){
+      return res.status(200).json(data)
+    })
+
+	} catch (error) {
+		return res.status(404).json({ error: 'Error while fetching data' })
+	}
 }
