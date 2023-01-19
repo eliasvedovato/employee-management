102823c8eccffb41ee7531244ff3a27c8374e4a0
Elias Vedovato <eliasvedovato@gmail.com>
2023-01-19 18:03:15 -0300

    middleware

---
+++ redux/listener.js
@@ -0,0 +1,15 @@
+//this middleware is listen to the toggle change action
+
+import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { toggleChangeAction, updateAction } from "./reducer";
+
+const listenerMiddleware = createListenerMiddleware()
+
+listenerMiddleware.startListening({
+  actionCreator: toggleChangeAction,
+  effect: async (action, listenerApi) => {
+    listenerApi.dispatch(updateAction(action.payload))
+  }
+})
+
+export default listenerMiddleware
